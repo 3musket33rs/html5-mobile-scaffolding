58cce630f63ea4a167809c066c9ee36737f20e10
corinne <fabricematrat@gmail.com>
2012-08-01 21:21:36 +0200

    licence Apache

--- Html5MobileScaffoldingGrailsPlugin.groovy
+++ Html5MobileScaffoldingGrailsPlugin.groovy
@@ -1,10 +1,32 @@
+/* Copyright 2012 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+*
+* @author <a href='mailto:th33musk3t33rs@gmail.com'>3.musketeers</a>
+*
+* @since 0.1
+*/
+
 import org.grails.html.mobile.JavascriptDateRegistrar
 
 class Html5MobileScaffoldingGrailsPlugin {
 
-    def version = '0.1-SNAPSHOT'
+    def version = '0.1'
     def grailsVersion = '2.0 > *'
-    def dependsOn = [:] //TODO to be added once published ['jsonp': "* > 2.0"]
+    def dependsOn = [:] //TODO to be added once published ['jsonp': "* > 0.01"]
     def pluginExcludes = []
 
     def title = 'Html5 Mobile Scaffolding Plugin'
