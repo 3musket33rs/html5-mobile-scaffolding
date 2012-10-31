3261d05aca870b128ebad90707018015cc6a2df9
corinnekrych <corinnekrych@gmail.com>
2012-10-31 23:26:42 +0100

    new doc

--- Html5MobileScaffoldingGrailsPlugin.groovy
+++ Html5MobileScaffoldingGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import org.grails.html.mobile.JavascriptDateRegistrar
 
 class Html5MobileScaffoldingGrailsPlugin {
 
-    def version = '0.1-SNAPSHOT'
+    def version = '0.3'
     def grailsVersion = '2.0 > *'
     def dependsOn = [:]
     def pluginExcludes = []
