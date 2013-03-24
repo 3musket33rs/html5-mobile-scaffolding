d39e8f863d63bbed3dae87c06752d16ea4a40c2e
corinnekrych <corinnekrych@gmail.com>
2013-03-24 23:45:39 +0100

    getcookie method

--- src/templates/scaffolding/js/grails/mobile/helper/toObject.js
+++ src/templates/scaffolding/js/grails/mobile/helper/toObject.js
@@ -42,6 +42,25 @@ grails.mobile.helper.toDomainObject = function (objectString) {
     return listDomainObject;
 };
 
+grails.mobile.helper.getCookie = function(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    return unescape(dc.substring(begin + prefix.length, end));
+}
+
 grails.mobile.helper.toObject = function (inputs) {
     var objectData;
     objectData = {};
