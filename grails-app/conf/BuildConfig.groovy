ee209b0b7396e0b988a13c6375ff6667654a1538
corinnekrych <corinnekrych@gmail.com>
2013-03-26 13:05:33 +0100

    fixing dependecies without snapshot for atmosphere plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,14 +22,14 @@ grails.project.dependency.resolution = {
         //mavenRepo "https://oss.sonatype.org/content/repositories/snapshots"
     }
     dependencies {
-//        compile('org.atmosphere:atmosphere-runtime:1.1.0.beta3') {
-//            excludes 'slf4j-api', 'atmosphere-ping'
-//        }
+        compile('org.atmosphere:atmosphere-runtime:1.1.0.beta3') {
+            excludes 'slf4j-api', 'atmosphere-ping'
+        }
     }
 
     plugins {
         runtime ":jquery:1.9.1", ":cors:1.0.3"
 //        runtime ":events-si:1.0.M7"
-        //compile ":events-push:1.0.M7"
+        compile ":events-push:1.0.M7"
     }
 }
